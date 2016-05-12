83e25eb1bb4137d141f7515ade7a4cec48f69b88
Casey Gollan <hello@caseyagollan.com>
2016-05-11 23:35:42 -0400

    microsite pass

---
+++ featuring.md
@@ -0,0 +1,4 @@
+---
+title: Featuring
+layout: featuring
+---
--- js/plugins.js
+++ js/plugins.js
@@ -22,3 +22,27 @@
 }());
 
 // Place any jQuery/helper plugins in here.
+
+$('#exampleModal').on('show.bs.modal', function (event) {
+  var button = $(event.relatedTarget) // Button that triggered the modal
+  var item = button.data('item') // Extract info from data-* attributes
+  var amount = button.data('amount') // Extract info from data-* attributes
+  // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+  // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+  var modal = $(this)
+  modal.find('.modal-item').text(item)
+  modal.find('.modal-amount').text('$' + amount)
+  modal.find('.donate-button').val('Donate $' + amount + ' at Fractured Atlas')
+  modal.find('.form-item').val(item)
+  modal.find('.form-amount').val(amount)
+})
+
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+})
+
+$("#countdown").countdown("2016/06/05", function(event) {
+  $(this).text(
+    event.strftime('%D days until the block party!')
+  );
+});
